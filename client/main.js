04c909101a989bbc296acb2b8084419f4c0f77a1
Kiyo <user@USER-no-MacBook-Air.local>
2017-05-21 07:58:49 +0900

    initial commit

---
+++ client/main.js
@@ -0,0 +1,10 @@
+import angulae from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
+
+angular.module('simple-todos', [
+    angularMeteor, 
+    todosList.name,
+    'accounts.ui'
+]);
